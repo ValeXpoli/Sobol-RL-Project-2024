f2ba0482fbf81e3e58a56a2fcf346aeb1da948c9
non-greek <andrea.grieco@gmx.com>
2024-09-10 12:20:38 +0200

    Added m1_plots mode in sens.py, noise masses and fixed some warnings

--- PPO_Sobol_sensitivity_analysis/sens.py
+++ PPO_Sobol_sensitivity_analysis/sens.py
@@ -22,16 +22,6 @@ import pandas as pd
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-# Register the custom environment globally
-def register_custom_envs():
-    register(
-        id='CustomHopper-source-v0',
-        entry_point='env.custom_hopper:CustomHopper',
-        kwargs={'domain': 'source'},
-        max_episode_steps=500,
-    )
-
-register_custom_envs()  # Register once at the top
 
 # Parse command line arguments
 def parse_args():
@@ -47,8 +37,9 @@ def parse_args():
     parser.add_argument("--range", default=5, type=float, help="Range for the UDR")
     parser.add_argument("--parallel", default=8, type=int, help="Number of parallel processes")
     parser.add_argument("--n", default=1000, type=int, help="Number of samples")
-    parser.add_argument("--mode", default="sobol", type=str, help="[sobol, linear]")
+    parser.add_argument("--mode", default="sobol", type=str, help="[sobol, linear, m1_plots]")
     parser.add_argument("--mass", default=None, type=int, help="[1,2,3]")
+    parser.add_argument("--noise", default=2, type=int, help="Number of noise variables")
     return parser.parse_args()
 
 args = parse_args()
@@ -62,8 +53,9 @@ def load_model_in_process(alg: str, model_name: str, device: str) -> Any:
     return model
 
 def test_model(sample_batch: np.ndarray, baseline: float, env_name: str, model_name: str, alg: str, device: str, test_episodes: int, batch_index: int) -> np.ndarray:
+    env_name = f"Batch{batch_index}-{env_name}"
     register(
-            id='CustomHopper-source-v0',
+            id=env_name,
             entry_point='env.custom_hopper:CustomHopper',
             kwargs={'domain': 'source'},
             max_episode_steps=500,
@@ -78,8 +70,11 @@ def test_model(sample_batch: np.ndarray, baseline: float, env_name: str, model_n
     rewards = []
     
     for i, sample in enumerate(sample_batch):
+        if args.noise > 0:
+            sample = sample[:-args.noise]
         # Assign the sample to the environment's body_mass
-        env.sim.model.body_mass[2:] = sample
+        env.sim.model.body_mass[2:2+len(sample)] = sample
+
         
         # Evaluate the policy and store the mean reward
         mean_reward, _ = evaluate_policy(model, env, n_eval_episodes=test_episodes, deterministic=True)
@@ -121,6 +116,7 @@ def compute_names(alg: str, train_type: str, dr_type: str) -> str:
     else:
         return f"./bestmodel/models/{model_name}"
 
+
 def loop_sobol(n: int, n_parallel: int) -> Dict[str, Any]:
     if n < 0 or n_parallel < 0:
         raise ValueError("The number of samples and parallel processes must be positive.")
@@ -128,10 +124,15 @@ def loop_sobol(n: int, n_parallel: int) -> Dict[str, Any]:
     model_name = compute_names(args.algorithm, "source", args.DR)
     env_name = "CustomHopper-source-v0"
     minimum, maximum = compute_hopper_mass_bounds()
+    bounds = [[minimum[0], maximum[0]], [minimum[1], maximum[1]], [minimum[2], maximum[2]]]
+    names = ["mass_1", "mass_2", "mass_3"]
+    for i in range(args.noise):
+        bounds.append([1,7])
+        names.append(f"noise_mass_{i+1}")
     problem = {
-        "num_vars": 3,
-        "names": ["mass_1", "mass_2", "mass_3"],
-        "bounds": [[minimum[0], maximum[0]], [minimum[1], maximum[1]], [minimum[2], maximum[2]]],
+        "num_vars": 3+args.noise,
+        "names": names,
+        "bounds": bounds
     }
     X = saltelli.sample(problem, N=n, calc_second_order=True)
     Y = parallel_evaluate(X, baseline=0, env_name=env_name, model_name=model_name, alg=args.algorithm, device=args.device, test_episodes=args.test_episodes, n_parallel=n_parallel)
@@ -139,13 +140,21 @@ def loop_sobol(n: int, n_parallel: int) -> Dict[str, Any]:
     return sobol_result
 
 def linear(n: int, n_parallel: int):
+    if n < 0 or n_parallel < 0:
+        raise ValueError("The number of samples and parallel processes must be positive.")
+
     model_name = compute_names(args.algorithm, "source", args.DR)
     env_name = "CustomHopper-source-v0"
     minimum, maximum = compute_hopper_mass_bounds()
+    bounds = [[minimum[0], maximum[0]], [minimum[1], maximum[1]], [minimum[2], maximum[2]]]
+    names = ["mass_1", "mass_2", "mass_3"]
+    for i in range(args.noise):
+        bounds.append([1,7])
+        names.append(f"noise_mass_{i+1}")
     problem = {
-        "num_vars": 3,
-        "names": ["mass_1", "mass_2", "mass_3"],
-        "bounds": [[minimum[0], maximum[0]], [minimum[1], maximum[1]], [minimum[2], maximum[2]]],
+        "num_vars": 3+args.noise,
+        "names": names,
+        "bounds": bounds
     }
     
     # Latin Hypercube Sampling for linear regression
@@ -153,19 +162,54 @@ def linear(n: int, n_parallel: int):
     
     # Parallel evaluation
     Y = parallel_evaluate(X, baseline=0, env_name=env_name, model_name=model_name, alg=args.algorithm, device=args.device, test_episodes=args.test_episodes, n_parallel=n_parallel)
+
+    avg_values = np.concatenate((np.array(gym.make(env_name).get_parameters()[1:]), np.array([4,4], dtype=np.float64)))
     
-    delta = sm.add_constant(np.abs(X - np.array(gym.make(env_name).get_parameters()[1:])))
+    #print(avg_values)
+    delta = sm.add_constant(np.abs(X -avg_values))
+
+    #print(delta)
     
-    plt.scatter(X[:, 0], Y, s=0.8)
+    model = sm.OLS(Y, delta)
+    res = model.fit_regularized()
+    results = model.fit(params=res.params)
+
+
+    df = pd.DataFrame(data=X, columns=["mass1", "mass2", "mass3", "noise1", "noise2"])
+    df.insert(value=Y, column="Y", loc=5)
+    #print(df)
+    #df.to_csv("./logsens/linear.csv", index=False)
+    print(results.summary())
+
+def make_plots(n, n_parallel):
+
+    model_name = compute_names(args.algorithm, "source", args.DR)
+    env_name = "CustomHopper-source-v0"
+    minimum, maximum = compute_hopper_mass_bounds()
+    problem = {
+        "num_vars": 1,
+        "names": ["mass_1"],
+        "bounds": [[minimum[0], maximum[0]]],
+    }
+    
+    # Latin Hypercube Sampling for linear regression
+    X = latin.sample(problem, n)
+    # Parallel evaluation
+    Y = parallel_evaluate(X, baseline=0, env_name=env_name, model_name=model_name, alg=args.algorithm, device=args.device, test_episodes=args.test_episodes, n_parallel=n_parallel)
+    
+    delta = sm.add_constant(np.abs(X - np.array(gym.make(env_name).get_parameters()[1])))
+    
+    plt.scatter(X, Y, s=2)
     plt.ylabel("Reward")
     plt.xlabel("Mass 1")
     
-    XY_sorted = list(zip(X[:, 0], Y))
+    XY_sorted = list(zip(X, Y))
     XY_sorted.sort(key=lambda x: x[0])
-    XY_sorted = np.array(XY_sorted)
-    centroids = np.zeros((5, 2))
+    XY_sorted = np.array(XY_sorted, dtype="object")
+    centroids = np.zeros((3, 2))
+    centroids_string = "["
     index = 0
-    for i in range(5):
+    for i in range(3):
         step = math.ceil(n / 3)
         if index + step < n:
             X_centroid = np.mean(XY_sorted[index : index + step, 0])
@@ -176,8 +220,11 @@ def linear(n: int, n_parallel: int):
             Y_centroid = np.mean(XY_sorted[index:, 1])
         centroids[i, 0] = X_centroid
         centroids[i, 1] = Y_centroid
-    
-    print(centroids)
+        if(i!=2):
+            centroids_string += f"({X_centroid[0]}, {Y_centroid}), "
+        else:
+            centroids_string += f"({X_centroid[0]}, {Y_centroid})]."
+    print("Centroids:", centroids_string)
     plt.scatter(centroids[:, 0], centroids[:, 1], marker="x", c="red", label="Centroids of the left, middle and upper portion")
     plt.axvline(gym.make(env_name).get_parameters()[1], alpha=0.5, ls="--", c="green", linewidth=1.5, label="Original value")
     plt.legend()
@@ -189,7 +236,6 @@ def linear(n: int, n_parallel: int):
     res = model.fit_regularized()
     results = model.fit(params=res.params)
 
-    print(results.params)
 
     plt.scatter(delta[:, 1], Y, s=0.8)
     plt.axline((0, results.params[0]), slope=results.params[1], linewidth=1.5, ls="--", c="red", label="Regression line")
@@ -200,19 +246,17 @@ def linear(n: int, n_parallel: int):
     plt.savefig("./plots/delta_m1_vs_reward.png")
     plt.show()
 
-    df = pd.DataFrame(data=X, columns=["mass1", "mass2", "mass3"])
-    df.insert(value=Y, column="Y", loc=3)
-    print(df)
-    df.to_csv("./logsens/linear.csv", index=False)
     print(results.summary())
 
 def main() -> Any:
     start_time = time.time()
     try:
         if args.mode == "sobol":
-            result = loop_sobol(args.n, args.parallel)
+            loop_sobol(args.n, args.parallel)
         elif args.mode == "linear":
             linear(args.n, args.parallel)
+        elif args.mode == "m1_plots":
+            make_plots(args.n, args.parallel)
         else:
             raise ValueError(f"Invalid mode: {args.mode}")
     except Exception as e:
@@ -220,7 +264,6 @@ def main() -> Any:
         raise
     finally:
         logger.info(f"Execution time: {time.time() - start_time:.2f} seconds")
-    return result
 
 if __name__ == "__main__":
     main()
